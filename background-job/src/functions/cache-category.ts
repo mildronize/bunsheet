a3d9b5dbb7f42146ed9f77e9d849b969acbaaa3b
Thada Wangthammang <thada.wth@gmail.com>
2024-04-13 00:34:15 +0700

    fix(bg-job): make debug func only acess by authLevel function

--- background-job/src/functions/cache-category.ts
+++ background-job/src/functions/cache-category.ts
@@ -2,19 +2,23 @@ import { GoogleSpreadsheet } from 'google-spreadsheet';
 import { func } from '../nammatham';
 import { sheetClient, sheetDoc } from '../bootstrap';
 
-export default func.httpGet('cacheCategory').handler(async c => {
-  const context = c.context;
+export default func
+  .httpGet('cacheCategory', {
+    authLevel: 'function',
+  })
+  .handler(async c => {
+    const context = c.context;
 
-  const asyncData = sheetClient.transaction.readAll();
+    const asyncData = sheetClient.transaction.readAll();
 
-  const data = [];
-  for await (const item of asyncData) {
-    data.push(item);
-  }
-  // const data = await readRows(sheetDoc, 681580727);
-  return c.json({
-    data,
-    length: data.length,
-    message: 'OK',
+    const data = [];
+    for await (const item of asyncData) {
+      data.push(item);
+    }
+    // const data = await readRows(sheetDoc, 681580727);
+    return c.json({
+      data,
+      length: data.length,
+      message: 'OK',
+    });
   });
-});
