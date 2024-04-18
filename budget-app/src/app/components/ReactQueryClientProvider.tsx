834c6783b8a6e6c2915103f1d396876686a3f35c
Thada Wangthammang <thada.wth@gmail.com>
2024-04-18 22:47:29 +0700

    feat(budget-app): disable react query cache, until find a way to invalidate it

--- budget-app/src/app/components/ReactQueryClientProvider.tsx
+++ budget-app/src/app/components/ReactQueryClientProvider.tsx
@@ -1,27 +1,29 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client'
-import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister'
+// import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client'
+// import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister'
 
 
-const persister = createSyncStoragePersister({
-  storage: typeof window !== 'undefined' ? window.localStorage : undefined,
-})
+// const persister = createSyncStoragePersister({
+//   storage: typeof window !== 'undefined' ? window.localStorage : undefined,
+// })
 
 
-export const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      gcTime: 1000 * 60 * 60, // 24 hours
-    },
-  },
-});
+// export const queryClient = new QueryClient({
+//   defaultOptions: {
+//     queries: {
+//       gcTime: 1000 * 60 * 60, // 24 hours
+//     },
+//   },
+// });
+
+export const queryClient = new QueryClient();
 
 export function ReactQueryClientProvider(props: { children: React.ReactNode }) {
   return (
-    <PersistQueryClientProvider client={queryClient}  persistOptions={{ persister }}>
+    <QueryClientProvider client={queryClient} >
       {props.children}
-    </PersistQueryClientProvider>
+    </QueryClientProvider>
   );
 }
